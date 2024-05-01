6c70f85ac02fd1d6e057a3e179f8a5b7468a4654
bnordlin <bnordlin@villanova.edu>
2024-05-01 18:24:26 -0400

    Initial application, non-functioning search bar and no cursor change on button click

---
+++ first-app/src/app/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousinglocationComponent } from '../housinglocation/housinglocation.component';
+import { Housinglocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousinglocationComponent],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housinglocation *ngFor="let housingLocation of housingLocationList"
+        [housingLocation]="housingLocation"></app-housinglocation>
+    </section>
+
+  `,
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  housingLocationList: Housinglocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList: Housinglocation[] = [];
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+
+}
